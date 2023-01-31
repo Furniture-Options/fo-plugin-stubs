281c44b6e0c14cdbc7d1fb08f52a6f232fd6492c
jesse-kaufman <jesse@jessekaufman.com>
2023-01-31 15:04:18 -0600

    Fixes to stub generation

--- tools/generate.php
+++ tools/generate.php
@@ -1,15 +1,17 @@
 <?php
-require 'vendor/autoload.php';
+
+require __DIR__ . '/../vendor/autoload.php';
 
 use StubsGenerator\StubsGenerator;
 use Symfony\Component\Finder\Finder;
 
 if ( ! isset( $argv[1] ) ) {
-	die( "\n" . 'You must provide the WordPress plugins directory path.'
+	die(
+		"\n" . 'You must provide the full path to the WordPress plugins directory.'
 		. "\n\n"
 		. "\t" . 'php generate.php [wp_plugin_path]'
 		. "\n\n"
-    	);
+	);
 }
 
 $plugins_path = realpath( $argv[1] );
@@ -26,26 +28,27 @@ $generator = new StubsGenerator();
 // generate stub files for all FO plugins
 $finder = new Finder();
 $finder->directories()
-	  ->name('fo-*')
-	  ->in($plugins_path);
+	   ->name( 'fo-*' )
+	   ->in( $plugins_path )
+	   ->ignoreVCSIgnored( true );
 
 if ( $finder->hasResults() ) {
-	foreach ($finder as $dir) {
+	foreach ( $finder as $dir ) {
 		$absolute_dir_path = $dir->getRealPath();
-		$plugin = $dir->getFilename();
+		$plugin            = $dir->getFilename();
 
 		echo "{$absolute_dir_path} : {$plugin}\n";
 
 		// Create a `StubsGenerator\Finder` which contains
 		// the set of files you wish to generate stubs for.
-		$plugin_finder = Finder::create()->in($absolute_dir_path)->ignoreVCSIgnored(true);
+		$plugin_finder = Finder::create()->in( $absolute_dir_path )->ignoreVCSIgnored( true );
 
 		// Now use the `StubsGenerator::generate()` method,
 		// which will return a `StubsGenerator\Result` instance.
-		$result = $generator->generate($plugin_finder);
+		$result = $generator->generate( $plugin_finder );
 
 		// Save stubs to file.
-		file_put_contents( 'stubs/' . $plugin . '.php', $result->prettyPrint() );
+		file_put_contents( '../stubs/' . $plugin . '.php', $result->prettyPrint() );
 	}
 }
 
@@ -53,13 +56,12 @@ if ( $finder->hasResults() ) {
 // Get some files from theme
 $theme_finder = new Finder();
 $theme_finder->files()
-		   ->name( array('functions.php', 'woocommerce.php', 'admin.php' ) )
-		   ->ignoreVCSIgnored( true )
+		   ->name( array( 'functions.php', 'woocommerce.php', 'admin.php' ) )
 		   ->in( $theme_dir );
 
 // Now use the `StubsGenerator::generate()` method,
 // which will return a `StubsGenerator\Result` instance.
-$result = $generator->generate($theme_finder);
+/* $result = $generator->generate( $theme_finder ); */
 
 // Save stubs to file.
-file_put_contents( 'stubs/fo-wp-theme.php', $result->prettyPrint() );
+/* file_put_contents( 'stubs/fo-wp-theme.php', $result->prettyPrint() ); */
